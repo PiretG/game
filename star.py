a18608faddf30eaea4b3add1d889598831cc0b1b
pgorban <pgorban@twilio.com>
2022-02-25 14:10:55 +0200

    Add level

--- star.py
+++ star.py
@@ -14,10 +14,7 @@ class Star(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midtop=(x, y))
 
     def update(self, height, width, ship_x, ship_y):
-        if self.rect.y >= height or self.collision(ship_x, ship_y):
+        if self.rect.y >= height:
             self.appear(random.randint(10, width), 5)
         else:
             self.rect.y += self.speed
-
-    def collision(self, ship_x, ship_y):
-        return abs(self.rect.x - ship_x) <= 20 and abs(self.rect.y - ship_y) <= 20
